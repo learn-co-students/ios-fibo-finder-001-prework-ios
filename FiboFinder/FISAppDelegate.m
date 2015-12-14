c09fcecb63cebada8ec59499e4568e8dbee4e6d8
Dnld <steinert.donald@gmail.com>
2015-12-14 17:49:49 -0500

    implements fibo finder

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -15,4 +15,24 @@
  
  */
 
+- (NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index
+{
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        } else if (i == 1) {
+            [sequence addObject:@1];
+        } else {
+            NSUInteger firstPrev = [sequence[i - 1] integerValue];
+            NSUInteger secondPrev = [sequence[i - 2] integerValue];
+            NSNumber *temp = @(firstPrev + secondPrev);
+            [sequence addObject:temp];
+        }
+    }
+    
+    return sequence;
+}
+
 @end
