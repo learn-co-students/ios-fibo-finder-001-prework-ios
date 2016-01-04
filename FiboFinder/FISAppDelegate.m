a3f2aac3abbcba1aaa9d87337aa381c0e0beb556
jeremyfeld <jeremyfeld@gmail.com>
2016-01-03 22:42:11 -0500

    create method to add next number in fibonacci sequence to array

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -15,4 +15,27 @@
  
  */
 
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    for (NSUInteger i =0; i < index+1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }
+        else if (i == 1) {
+            [sequence addObject:@1];
+        }
+        else {
+            NSNumber *one = sequence[i-1];
+            NSNumber *two = sequence[i-2];
+            NSInteger oneInt = [one integerValue];
+            NSInteger twoInt = [two integerValue];
+            NSInteger nextFibInt = oneInt + twoInt;
+            NSNumber *nextFib = @(nextFibInt);
+            [sequence addObject:nextFib];
+        }
+    }
+    NSLog(@"%@", sequence);
+    return sequence;
+}
+
 @end
