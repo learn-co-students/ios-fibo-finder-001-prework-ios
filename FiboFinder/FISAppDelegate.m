1fc692aee865de665154140c375d7aa830a5bf31
fleish007 <cfleish@gmail.com>
2016-01-05 12:27:40 -0500

    Fibonacci finder exercise.

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -6,6 +6,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
     return YES;
 }
 
@@ -14,5 +16,27 @@
  * Write your method implemenation here.
  
  */
+
+- (NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }
+        else if (i == 1) {
+            [sequence addObject:@1];
+        }
+        else {
+            NSInteger priorNum = [sequence[i-1] integerValue];
+            NSInteger twoPriorNum = [sequence[i-2] integerValue];
+            NSNumber *currentNum = @(priorNum + twoPriorNum);
+            [sequence addObject:currentNum];
+        }
+    }
+    
+    
+    return sequence;
+}
 
 @end
