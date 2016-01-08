523f05913543d84e5de9aaef2e7fbccb9ac34112
Cordavi <michaelamundsen@gmail.com>
2016-01-07 22:38:49 -0500

    Uploading completed Fibonacci lab

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -6,13 +6,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        } else if (i == 1) {
+            [sequence addObject:@1];
+        } else {
+            NSNumber *sum = @([sequence[i-1] unsignedIntegerValue] + [sequence[i-2] unsignedIntegerValue]);
+            [sequence addObject:sum];
+        }
+    }
+    return sequence;
+}
 
 @end
