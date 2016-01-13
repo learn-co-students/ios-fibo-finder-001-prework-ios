966b6f76030240a2c0c184b86d748e1ebc567c6d
eoinwhitney <whitneyeoin@gmail.com>
2016-01-13 14:23:14 -0500

    fibonacci sequence

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,30 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+
+-(NSArray*)arrayWithFibonacciSequenceToIndex:(NSUInteger)index; {
+    NSMutableArray *sequence= [[NSMutableArray alloc] init];
+    for(NSUInteger i=0;i<index+1;i++) {
+        NSNumber *fibStart=@(i);
+        if([fibStart isEqualToNumber:@0]) {
+            [sequence addObject:fibStart];
+            }
+        else if([fibStart isEqualToNumber:@1]) {
+             [sequence addObject:fibStart];
+        }
+        else {
+            NSNumber *fibOne=sequence[i-1];
+            NSNumber *fibTwo=sequence[i-2];
+            NSUInteger fibOneInt=[fibOne integerValue];
+            NSUInteger fibTwoInt=[fibTwo integerValue];
+            NSUInteger nextNumber= fibOneInt + fibTwoInt;
+            NSNumber *nextNumberObject=@(nextNumber);
+            [sequence addObject:nextNumberObject];
+        }
+    }
+    return sequence;
+}
+
+
 
 @end
