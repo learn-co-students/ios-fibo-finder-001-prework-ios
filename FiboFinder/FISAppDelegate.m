1fa524e8a6d4146b4840f6415ce3b7d8970e6bd7
bclous <brian.clouser@gmail.com>
2016-01-13 11:28:19 -0500

    finished lab all tests passed

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -15,4 +15,37 @@
  
  */
 
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index{
+    
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for(NSUInteger i =0; i < index +1; i++)
+    {
+        NSNumber *indexZero = @0;
+        NSNumber *indexOne = @1;
+        
+        
+        if(i ==0)
+        {
+            [sequence addObject:indexZero];
+        }
+        else if ( i==1 )
+        {
+            [sequence addObject:indexOne];
+        }
+        else
+        {
+            NSInteger valueOneBeforeInt = [sequence[i-1] integerValue];
+            NSInteger valueTwoBeforeInt = [sequence[i-2] integerValue];
+            NSInteger SumOfPreviousTwoValuesInt = valueOneBeforeInt + valueTwoBeforeInt;
+            NSNumber *SumofPreviousTwoValues = @(SumOfPreviousTwoValuesInt);
+            
+            [sequence addObject:SumofPreviousTwoValues];
+        }
+    }
+    
+    return sequence;
+}
+
 @end
