e41f2222a7abf0a09a55c2ffcc4e8901c6edcde4
Dougly <dgalante11@gmail.com>
2016-03-01 14:25:28 -0500

    local tests pass

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -14,5 +14,23 @@
  * Write your method implemenation here.
  
  */
+- (NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0){
+            [sequence addObject:@0];
+        }else if (i == 1){
+            [sequence addObject:@1];
+        }else{
+            NSNumber *twoBack = sequence[i-2];
+            NSNumber *oneBack = sequence[i-1];
+            NSInteger twoBackInt = [twoBack integerValue];
+            NSInteger oneBackInt = [oneBack integerValue];
+            NSNumber *nextInSequence = @(twoBackInt + oneBackInt);
+            [sequence addObject:nextInSequence];
+        }
+    }
+    return sequence;
+}
 
 @end
