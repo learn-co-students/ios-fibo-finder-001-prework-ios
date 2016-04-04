1fb698bd747134e20e9769dfb4f743b83af4a1e8
hdinhof1 <hdinhofer@gmail.com>
2016-04-04 16:43:29 -0400

    Done.

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -14,5 +14,28 @@
  * Write your method implemenation here.
  
  */
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    //Fibonacci using dynamic programming approach (not space efficient)
+    for (NSUInteger i =0; i< index+1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }
+        else if (i == 1) {
+            [sequence addObject:@1];
+        }else {
+            NSNumber *nMinusTwo = sequence[i-2];
+            NSNumber *nMinusOne = sequence[i-1];
+            NSUInteger nMinusTwoInt = [nMinusTwo integerValue];
+            NSUInteger nMinusOneInt = [nMinusOne integerValue];
+            NSUInteger sum = nMinusTwoInt + nMinusOneInt;
+            [sequence addObject:@(sum)];
+        }
+    }
+    
+    
+    return sequence;
+}
 
 @end
