1487b1d6d80a9a14f49392c0176929181bd796e1
github username <github email address>
2016-04-10 18:19:46 -0400

    lab is complete

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,23 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+// implementation of the fibonacci method
+- (NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }
+        else if (i == 1){
+            [sequence addObject:@1];
+        }
+        else {
+            NSUInteger fibonacciIntegerforCurrentIndex = [sequence[i-1] integerValue] + [sequence[i-2] integerValue];
+            [sequence addObject:@(fibonacciIntegerforCurrentIndex)];
+        }
+    }
+    return sequence;
+}
+
 
 @end
