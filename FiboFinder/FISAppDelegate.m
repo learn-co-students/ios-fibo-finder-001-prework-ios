d702f0500cc0e04ce1753a5a7aa08ac145bd2126
bsoung <bsoung@gm.slc.edu>
2016-04-30 23:39:34 -0400

    first commit

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,31 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+- (NSArray *)arrayWithFibonacciSequenceToIndex: (NSUInteger)index {
+    
+    NSMutableArray *sequence = [NSMutableArray array];
+
+    NSUInteger i = 0;
+    for (i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        } else if (i == 1) {
+            [sequence addObject:@1];
+        } else {
+            NSNumber *a = sequence[i - 1];
+            NSNumber *b = sequence[i - 2];
+            
+            NSUInteger intA = [a integerValue];
+            NSUInteger intB = [b integerValue];
+            NSUInteger sum = intA + intB;
+            
+            NSNumber *newSum = [NSNumber numberWithInteger:sum];
+            [sequence addObject:newSum];
+        }
+    }
+    
+    
+    return  sequence;
+}
 
 @end
