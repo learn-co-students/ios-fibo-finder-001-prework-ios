7147f7bae9635533555123cac421a8c2e56aea00
OliviaGunton <olivia.gunton@gmail.com>
2016-07-01 12:30:23 -0400

    Implement arrayWithFibonacciSequenceToIndex

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,22 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+- (NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index{
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i <= index; i++){
+        if(i == 0){
+            [sequence addObject:@0];
+        }else if(i == 1){
+            [sequence addObject:@1];
+        }else{
+            NSInteger toAdd = [sequence[i-2] integerValue] + [sequence[i-1] integerValue];
+            [sequence addObject:@(toAdd)];
+        }
+    }
+    
+    return sequence;
+}
 
 @end
