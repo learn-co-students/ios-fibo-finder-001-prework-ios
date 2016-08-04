6442b0dbd5cdb58c5500f6a7f60d0ea812d09caa
Forrest Zhao <fzhao@prosper.com>
2016-08-04 15:07:25 -0700

    completed

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -15,4 +15,21 @@
  
  */
 
+- (NSArray *)arrayWithFibonacciSequenceToIndex: (NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }
+        else if (i == 1) {
+            [sequence addObject:@1];
+        }
+        else{
+            NSUInteger nextFibVal = [[sequence objectAtIndex:i-1] unsignedIntegerValue] + [[sequence objectAtIndex:i-2] unsignedIntegerValue];
+            [sequence addObject:@(nextFibVal)];
+        }
+    }
+    return sequence;
+}
+
 @end
