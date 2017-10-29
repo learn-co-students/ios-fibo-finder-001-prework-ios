01ae4980d822e022857b6e01bf08b70196bf7d37
hingyenyin <yenyin.262@gmail.com>
2017-10-29 23:25:46 +0800

    done

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,44 @@
     return YES;
 }
 
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    
+    NSMutableArray *sequence =   [[ NSMutableArray alloc] init];
+    
+    for(NSUInteger i = 0; i < index + 1; i++) {
+        
+        if(i == 0) {
+            
+            [sequence addObject:@0];
+            
+        }
+        else if (i == 1) {
+            
+            [sequence addObject:@1];
+            
+        }
+        else {
+            
+            
+                   NSUInteger primeA = [sequence[i-2] unsignedIntegerValue];
+                    NSUInteger primeB = [sequence[i-1] unsignedIntegerValue];
+            NSNumber *newFib = @(primeA + primeB);
+            [sequence addObject:newFib];
+            
+           
+        }
+    }
+    NSLog(@"%@", sequence);
+    
+    return sequence;
+    
+    
+}
 /**
  
  * Write your method implemenation here.
  
  */
 
-@end
+    @end
+    
